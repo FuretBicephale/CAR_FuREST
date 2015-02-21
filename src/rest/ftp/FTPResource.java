e26629000e8aead8680362301dacb0e7270a62e7
unknown <Phoenix-Left@hotmail.fr>
2015-02-21 16:57:44 +0100

    Try to differenciate CWD and RETR

--- src/rest/ftp/FTPResource.java
+++ src/rest/ftp/FTPResource.java
@@ -2,15 +2,15 @@ package rest.ftp;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.net.UnknownHostException;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import org.apache.commons.net.ftp.*;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 
 /**
  * @author cachera - falez
@@ -41,7 +41,7 @@ public class FTPResource {
 			for(int i = 0; i < dirContents.length; i++) {
 				if(dirContents[i].getName().endsWith("~"))
 					break;
-				htmlReturn += "<a href=\"" + dirContents[i].getName() + "\">" + dirContents[i] + "</a><br/>";						
+				htmlReturn += "<a href=\"ftp/" + dirContents[i].getName() + "\">" + dirContents[i] + "</a><br/>";						
 			}
 			
 		} catch (IOException e) {
@@ -55,15 +55,14 @@ public class FTPResource {
 	}
 	
 	/**
-	 * Send a GET Request to the FTP Server for the file pathname
+	 * Send a RETR Request to the FTP Server for the file pathname
 	 * @param pathname The file to retrieve
 	 * @return The file if it's found, null otherwise
 	 */
-
 	@GET
 	@Path("{path: .*}")
 	@Produces("application/html")
-	public byte[] resolvePathname( @PathParam("path") String pathname ) {
+	public byte[] resolvePathname(@PathParam("path") String pathname) {
 		
 		System.out.println("File = "+pathname);
 		FTPClient client = new FTPClient();
@@ -87,9 +86,45 @@ public class FTPResource {
 			System.err.println("Unable to connect to FTP Server.");
 			
 		}
+		
 		return null;
 		
 	}
-
+	
+	/**
+	 * Send a LIST Request to the FTP Server for the directory pathname
+	 * @param pathname The pathname to list
+	 * @return A String which contains HTML Code to display the directory content
+	 */
+	@POST
+	@Path("{path: .*}")
+	@Produces("text/html")
+	public String listDir(@PathParam("path") String pathname) {
+		
+		String htmlReturn = "";
+		FTPClient client = new FTPClient();
+		
+		try {
+			
+			client.connect("127.0.0.1", 4223);
+			client.login("user", "password");
+			
+			FTPFile[] dirContents = client.listFiles(pathname);
+			
+			for(int i = 0; i < dirContents.length; i++) {
+				if(dirContents[i].getName().endsWith("~"))
+					break;
+				htmlReturn += "<a href=\"ftp/" + pathname + dirContents[i].getName() + "\">" + dirContents[i] + "</a><br/>";						
+			}
+			
+		} catch (IOException e) {
+			
+			System.err.println("Unable to connect to FTP Server.");
+			
+		}
+		
+		return htmlReturn;
+		
+	}
 
 }
